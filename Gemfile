ace7c680812910a1be39bbc199e2821dcf51577a
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-01 11:18:34 +0900

    複雑な検索機能の実装

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'mini_magick'
 gem 'active_hash'
+gem 'ransack'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,19 @@ class ItemsController < ApplicationController
     end
   end
 
+  def search
+        # params[:q]がnilではない且つ、params[:q][:name]がnilではないとき（商品名の欄が入力されているとき）
+    # if params[:q] && params[:q][:name]と同じような意味合い
+    if params[:q]&.dig(:name)
+      # squishメソッドで余分なスペースを削除する
+      squished_keywords = params[:q][:name].squish
+      ## 半角スペースを区切り文字として配列を生成し、paramsに入れる
+      params[:q][:name_cont_any] = squished_keywords.split(" ")
+    end
+    @q = Item.ransack(params[:q])
+    @items = @q.result
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :image, :category_id, :price).merge(user_id: current_user.id)
